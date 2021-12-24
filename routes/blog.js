c62d875d2f9170d1aa8265b48573b0f6620e9a4b
Niek Mollers <info@sofane.nl>
2021-12-24 01:12:04 +0100

    432 Fetching & Displaying a Single Blog Post

--- routes/blog.js
+++ routes/blog.js
@@ -31,6 +31,20 @@ router.get('/posts', async function (req, res) {
   res.render('posts-list', { posts: posts });
 });
 
+router.get('/posts/:id', async function (req, res) {
+  const query = `
+    SELECT posts.*, authors.name AS authors_name, authors.email AS author_email FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ?
+  `;
+  const [posts] = await db.query(query, [req.params.id]);
+  if (!posts || posts.length === 0) {
+    return res.status(404).render('404');
+  }
+  console.log(posts);
+  res.render('post-detail', { post: posts[0] });
+});
+
 router.get('/new-post', async function (req, res) {
   const [authors] = await db.query('SELECT * FROM authors');
   res.render('create-post', { authors: authors });
